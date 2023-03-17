9f4b707fdd6231c4ce5b3b2e769e338f86bc33a7
Lim Chee Kin <limcheekin@vobject.com>
2023-03-17 15:25:31 +0800

    feat: fixed solhint no-global-import warning

--- contracts/test/VRFCoordinatorV2Mock.sol
+++ contracts/test/VRFCoordinatorV2Mock.sol
@@ -1,4 +1,4 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
-import "@chainlink/contracts/src/v0.8/mocks/VRFCoordinatorV2Mock.sol";
+import {VRFCoordinatorV2Mock} from "@chainlink/contracts/src/v0.8/mocks/VRFCoordinatorV2Mock.sol";
